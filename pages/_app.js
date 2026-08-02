1655fcfaeb5d3a7d357ad679d74b5adaccd22a6a
danielrss139-commits <danielrss139@gmail.com>
2026-08-02 10:45:45 -0300

    Enhance App component for PWA support
    Register service worker and add metadata for PWA.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch(() => {});
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0b1730" />
+        <link rel="icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+    }
